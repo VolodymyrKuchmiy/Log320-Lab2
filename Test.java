4c382f73654ab7258de15b0973461cd53d5d34af
VolodymyrKuchmiy <145402889+VolodymyrKuchmiy@users.noreply.github.com>
2024-02-16 19:39:23 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,7 +3,8 @@ public class Test {
     public static void main(String[] args) {
         Board board = new Board();
         board.afficherBoardPrint();
-        Case caseInitiale = new Case('A', 2, new Piece(4)); // pour tester que le mouvement de pieces fonctionne bien
+        Case caseInitiale = new Case('A', 2, new Piece(4)); // pour tester que le
+        // mouvement de pieces fonctionne bien
         Case caseFinale = new Case('B', 2, new Piece(4));
         System.out.println("En haut est un board initialise, on fait un move A2-B2:");
         board.movePiece(caseInitiale, caseFinale);
